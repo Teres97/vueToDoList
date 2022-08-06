c44a156259e00fb16f92fcca7bfa36e4c7add02b
Eduard Tereshchuk <edter@MacBook-Pro-Eduard.local>
2022-08-06 13:58:11 +0200

    computed

--- src/components/insertItem.vue
+++ src/components/insertItem.vue
@@ -1,30 +1,40 @@
 <template>
-<div v-for="(myNote,idx) in notes" :key="myNote">
-    <li :class="myNote.color">
-        {{idx+1 }} {{ myNote.message }} {{ myNote.deadline }}
-        <button @click="removeItem(idx)">Delete</button>
-    </li>
-</div>
-    <div class="flex">
-        <label class="l_description" for="descritpion">Descritpion</label>
-        <input class="description" id="description" name="descritpion" v-model="message" @keypress.enter="addItem">
+<div>
+    <label class="h3" for="descritpion">Descritpion</label>
+    <input class="ms-1 me-1 form-control" id="description" name="descritpion" v-model="message" @keypress.enter="addItem">
+    <div class="mt-2">
+        <div>
+            <label class="ms-1" for="important">Important</label>
+            <input class="ms-1" type="radio" id="green" value="green" v-model="color" checked>
+            <label class="green ms-1" for="green">Green</label>
+            <input type="radio" id="yellow" value="yellow" v-model="color">
+            <label class="yellow ms-1" for="yellow">Yellow</label>
+            <input type="radio" id="red" value="red" v-model="color">
+            <label class="red ms-1" for="red">Red</label>
+        </div>
         <div>
             <label for="deadline">Deadline</label>
-            <input type="checkbox" id="deadline" name="deadline" v-model="checked">
-            <input type="datetime-local" v-model="deadline" v-show="checked">
-            <label for="important">Important</label>
-            <input type="radio" id="green" value="green" v-model="color" checked>
-            <label class="green" for="green">Green</label>
-            <input type="radio" id="yellow" value="yellow" v-model="color">
-            <label class="yellow" for="yellow">Yellow</label>
-            <input type="radio" id="red" value="red" v-model="color">
-            <label class="red" for="red">Red</label>
-        </div>
-        <div>
-            <button class="btn btn-primary" @click="addItem">Submit</button>
-            <button class="btn btn-primary" @click="cancelItem">Cancel</button>
+            <input class="ms-1" type="checkbox" id="deadline" name="deadline" v-model="checked">
+            <input class="ms-1" type="datetime-local" v-model="deadline" v-show="checked">
         </div>
     </div>
+    <div class="mt-2">
+            <button type="button" class="btn btn-primary" @click="addItem">Submit</button>
+            <button type="button" class="btn btn-warning ms-1" @click="cancelItem">Cancel</button>
+    </div>
+</div>
+<div class="container" v-for="(myNote,idx) in notes" :key="myNote">
+    <li class="p-2 mt-1 d-flex rounded" :class="myNote.color">
+            <h3 class="me-3">
+            {{idx+1 }}
+            </h3> 
+            {{ myNote.message }} 
+            <div class="ms-auto ">
+                {{ myNote.deadline }}
+            </div>
+        <button type="button" class="btn btn-danger ms-auto" @click="removeItem(idx)">Delete</button>
+    </li>
+</div>
 </template>
 
 <script>
@@ -36,7 +46,6 @@ export default {
             message: "",
             notes: [],
             color: "green",
-            noteObj: {message: "", deadline: "", color: ""},
             checked: false,
         };
     },
@@ -44,12 +53,10 @@ export default {
         addItem() {
             if(this.message){
                 if(this.checked === false){
-                    this.noteObj = {message: this.message, color: this.color}
                     this.notes.push(this.noteObj);
                 }else{
-                    this.noteObj = {message: this.message, deadline: this.deadline, color: this.color}
-                    this.notes.push(this.noteObj);
-                }
+                    this.notes.push(this.noteObjDeadline);
+                }   
             }
 
             this.message = ""
@@ -62,6 +69,14 @@ export default {
             this.deadline = "0001-01-01T00:00"
         },
     },
+    computed:{
+        noteObj(){
+            return {message: this.message, color: this.color}
+        },
+        noteObjDeadline(){
+            return {message: this.message, deadline: this.deadline, color: this.color}
+        }
+    }
 }
 </script>
 
@@ -77,12 +92,5 @@ export default {
 .red{
     background-color: rgb(245, 83, 83);
     margin: 0 5px;
-}
-.description{
-    margin: 0 15%;
-}
-.flex{
-    display: flex;
-    flex-direction: column;
 }
 </style>
