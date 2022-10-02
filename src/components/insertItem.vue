b1d705c44561d245a755058472b85d8c9775fdfd
Eduard Tereshchuk <edter@MacBook-Pro-Eduard.local>
2022-10-02 21:11:06 +0200

    Dark/light theme

--- src/components/insertItem.vue
+++ src/components/insertItem.vue
@@ -1,4 +1,10 @@
 <template>
+<div class="theme">Dark Theme
+    <label class="switch">
+        <input @click="darkThemeSwitch" type="checkbox">
+        <span class="slider round"></span>
+    </label>
+</div>
 <div>
     <label class="h3" for="descritpion">Descritpion</label>
     <input class="ms-1 me-1 form-control" id="description" name="descritpion" v-model="message" @keypress.enter="addItem">
@@ -95,6 +101,28 @@ export default {
             this.idNote = null
             }
         },
+        addDarkTheme() {
+            let darkThemeLinkEl = document.createElement("link");
+            darkThemeLinkEl.setAttribute("rel", "stylesheet");
+            darkThemeLinkEl.setAttribute("href", "/css/dark.css");
+            darkThemeLinkEl.setAttribute("id", "dark");
+
+            let docHead = document.querySelector("head");
+            docHead.append(darkThemeLinkEl);
+        },
+        removeDarkTheme() {
+            let darkThemeLinkEl = document.querySelector("#dark");
+            let parentNode = darkThemeLinkEl.parentNode;
+            parentNode.removeChild(darkThemeLinkEl);
+        },
+        darkThemeSwitch() {
+            let darkThemeLinkEl = document.querySelector("#dark");
+            if (!darkThemeLinkEl) {
+              this.addDarkTheme()
+            } else {
+              this.removeDarkTheme()
+            }
+        },
     },
     computed:{
         noteObj(){
@@ -106,18 +134,70 @@ export default {
     }
 }
 </script>
-
 <style>
-.green{
-    background-color: rgb(83, 219, 83);
-    margin: 0 5px;
+.theme{
+  margin: 10px;
+  display: flex;
 }
-.yellow{
-    background-color: rgb(243, 243, 103);
-    margin: 0 5px;
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 30px;
+  height: 17px;
 }
-.red{
-    background-color: rgb(245, 83, 83);
-    margin: 0 5px;
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 13px;
+  width: 13px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(13px);
+  -ms-transform: translateX(13px);
+  transform: translateX(13px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 17px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
