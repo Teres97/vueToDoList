dec1027bf189c31988b440234b73b1fe935fc518
Eduard Tereshchuk <teres97@mail.ru>
2023-01-11 23:00:15 +0100

    404 Page not found

--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import components from '@/components/UI';
 import directives from './directives';
+import { createWebHistory, createRouter } from "vue-router"
+import PageNotFound from '@/pages/PageNotFound';
+import HomePage from '@/pages/HomePage'
+
+
+
+
+
+const routes = [
+    { path: '/', component: HomePage },
+    { path: '/:pathMatch(.*)*', component: PageNotFound },
+  ]
+  
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+})
 
 const app = createApp(App)
 
@@ -13,8 +31,7 @@ components.forEach(component => {
 directives.forEach(directive => {
     app.directive(directive.name, directive)
 })
-
-
+app.use(router)
 app.mount('#app')
 
 import 'bootstrap/dist/js/bootstrap.js'
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,85 @@
+<template>
+  <insert-item @add="addNote" />
+  <my-input 
+  v-model="searchQuery"
+  />
+  <sort-button style="margin-left: 10px" @click="sortedNotesMessage()">Sort Note</sort-button> 
+  <sort-button style="margin-left: 10px" @click="sortedNotesDate()">Sort Date</sort-button> 
+  <notes-list 
+  :notes="sortedAndSearchNotes" 
+  @remove="removeItem"
+  @update="updateItem" />
+</template>
+
+<script>
+import InsertItem from "@/components/InsertItem.vue";
+import NotesList from "@/components/NotesList.vue";
+import MyInput from '@/components/UI/MyInput.vue';
+
+export default {
+    name: "HomePage",
+  components: {
+    InsertItem,
+    NotesList,
+    MyInput,
+  },
+  data() {
+    return {
+      notes: [],
+      searchQuery: '',
+    };
+  },
+  mounted() {
+    if (localStorage.getItem("notes")) {
+      try {
+        this.notes = JSON.parse(localStorage.getItem("notes"));
+      } catch (e) {
+        localStorage.removeItem("notes");
+      }
+    }
+  },
+  methods: {
+
+    saveNotes() {
+      const parsed = JSON.stringify(this.notes);
+      localStorage.setItem("notes", parsed);
+    },
+
+    addNote(note) {
+      this.notes.push(note);
+      this.notes = this.notes.filter((p) => p.id);
+      this.saveNotes();
+    },
+
+    removeItem(note) {
+      this.notes = this.notes.filter((p) => p.id !== note.id);
+      this.saveNotes();
+    },
+
+    updateItem(note, index){
+      this.notes[index] = note;
+      this.saveNotes();
+    },
+
+    sortedNotesMessage(){
+      this.notes = this.notes.sort((note1, note2) => note1['message']?.localeCompare(note2['message']));
+      this.notes = this.notes.filter((p) => p.id);
+    },
+
+    sortedNotesDate(){
+      this.notes = this.notes.sort((note1, note2) => note1['deadline']?.localeCompare(note2['deadline']));
+      this.notes = this.notes.filter((p) => p.id);
+    },
+    
+  },
+  computed: {
+    sortedAndSearchNotes(){
+      return this.notes.filter(note => note.message.includes(this.searchQuery))
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
